017cabf48bf5f57841bc021dccec53474e2ddd0c
EAMarquezAs <ea.marquezas@alum.up.edu.pe>
2025-10-18 12:50:09 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from processing import df
+from viz import map_sin, map_fallecidos
 
+st.title("Prueba")
+
+st.write("Tabla")
+st.table(df)
+
+st.write("Mapas")
+st_folium(map_sin)
+st_folium(map_fallecidos)
