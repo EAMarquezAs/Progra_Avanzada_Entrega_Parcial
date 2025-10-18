075a5f3e79c7fa1dc70323957077bc2b5c77f569
EAMarquezAs <ea.marquezas@alum.up.edu.pe>
2025-10-18 12:46:06 -0500

    Update viz.py

--- src/viz.py
+++ src/viz.py
@@ -1,1 +1,14 @@
+from processing import df
 
+
+def mapa(df):
+  map = folium.Map(location=(df['LAT'].mean(), df['LON'].mean()), zoom_start=5)
+  cluster = MarkerCluster().add_to(map)
+  for index, row in df.iterrows():
+    if not pd.isna(row['LAT']) and not pd.isna(row['LON']):
+      folium.Marker(location=[row['LAT'], row['LON']]).add_to(cluster)
+  return map
+
+
+map_sin = mapa(df.drop_duplicates(subset=['CÓDIGO SINIESTRO']))
+map_fallecidos = mapa(df[df['GRAVEDAD']=='FALLECIDO'])
