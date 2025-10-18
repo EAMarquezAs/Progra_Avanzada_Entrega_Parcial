cec78c8092da026cb85972ff8bc11fe0f4e84af6
EAMarquezAs <ea.marquezas@alum.up.edu.pe>
2025-10-18 13:06:31 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 import streamlit as st
+import folium
 from processing import df
 from viz import map_sin, map_fallecidos
 
 st.title("Prueba")
 
 st.write("Tabla")
-st.table(df)
+st.table(df.head())
 
 st.write("Mapas")
-st_folium(map_sin)
-st_folium(map_fallecidos)
+st.components.v1.html(folium.Figure().add_child(map_sin).render(), height=500)
