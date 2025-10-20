6e27bbd6b3b648c3cb3375f4c86f57bc1976d7b5
EAMarquezAs <ea.marquezas@alum.up.edu.pe>
2025-10-19 23:17:29 -0500

    Update viz.py

--- src/viz.py
+++ src/viz.py
@@ -1,6 +1,7 @@
 import folium
 from folium.plugins import MarkerCluster
 import matplotlib.pyplot as plt
+import altair as alt
 import pandas as pd
 from processing import df
 
@@ -14,5 +15,26 @@ def mapa(df):
   return map
 
 
-map_sin = mapa(df.drop_duplicates(subset=['CÓDIGO SINIESTRO']))
-map_fallecidos = mapa(df[df['GRAVEDAD']=='FALLECIDO'])
+def barchart(df, colx, coly, titlex, titley, sortx=None, sorty=None):
+  chart = alt.Chart(df).mark_bar().encode(
+        # Ordenar el eje Y por la cantidad de fallecidos (FALLECIDOS)
+        y=alt.Y(coly, sort=sorty, title=titley),
+        x=alt.X(colx, sort=sortx, title=titlex),
+        tooltip=[coly, colx],
+        # Añadir texto con el valor exacto
+        text=alt.Text(colx) 
+  )
+  return chart
+
+
+def pie(df, categories, values):
+  chart = alt.Chart(df).encode(
+    theta=alt.Theta(f"{values}:Q", stack=True), color=alt.Color(f"{categories}:N")
+  ).mark_arc(outerRadius=110)
+  text = chart.mark_text(radius=140, size=15).encode(text=f"{values}:N")
+  return chart+text
+
+
+def hist(df, colx, titlex, titley):
+  chart = alt.Chart(df).mark_bar().encode(x=alt.X(colx, bin=True, title=titlex), y=alt.Y('count()', title=titley))
+  return chart
