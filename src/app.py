428f0860883c4a5c1a42fe8f49a7b3300a1d279d
EAMarquezAs <ea.marquezas@alum.up.edu.pe>
2025-10-20 13:45:43 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -19,15 +19,32 @@ st.caption("Fuente: https://www.onsv.gob.pe/estaticos/excel/BBDD%20ONSV%20-%20PE
 
 
 # Medidas
-st.write("Número de Datos sin Aplicar Filtros")
+st.write("Número de Datos Antes de Procesamiento")
 col1, col2 = st.columns(2)
 col1.metric(label="Total de Personas", value=total_per)
 col2.metric(label="Total de Siniestros", value=total_sin)
+
 
 # Filtro por Gravedad
 grav_pills = st.sidebar.pills("Gravedad", options=map(lambda x: x.capitalize(), df['GRAVEDAD'].unique()),
                         selection_mode='multi', default=map(lambda x: x.capitalize(), df['GRAVEDAD'].unique()))
 filter_grav = df['GRAVEDAD'].isin(map(lambda x: x.upper(), grav_pills))
+
+
+# Filtro por Sexo
+sex_radio = st.sidebar.segmented_control("Sexo", options=map(lambda x: x.capitalize(), df['SEXO'].unique()), selection_mode='multi',
+                                 default=map(lambda x: x.capitalize(), df['SEXO'].unique()))
+filter_sex = df['SEXO'].isin(map(lambda x: x.upper(), sex_radio))
+
+
+# Filtro por Tipo Persona
+tip_per = st.sidebar.multiselect("Tipo de Persona", options=map(lambda x: x.capitalize(), df['TIPO PERSONA'].unique()),
+                                 placeholder="All")
+if len(tip_per) == 0:
+        filter_tipper = True
+else:
+        filter_tipper = df['TIPO PERSONA'].isin(map(lambda x: x.upper(), tip_per))
+
 
 # Filtro por Fecha
 if 'sliderfecha' not in st.session_state:
@@ -40,8 +57,21 @@ box_fecha = st.sidebar.date_input(label="", min_value=datetime.date(2021, 1, 1),
                                 key='boxfecha', on_change=update, args=('sliderfecha', 'boxfecha', True))
 filter_fecha = (df['FECHA'] >= slider_fecha[0]) & (df['FECHA'] <= slider_fecha[1])
 
+
+# Descargar archivos originales
+st.sidebar.write('Archivos Originales')
+with open("data\BBDD ONSV - PERSONAS 2021-2023.xlsx", 'rb') as file:
+        st.sidebar.download_button("BBDD ONSV", data=file,
+                                   mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                   file_name="BBDD ONSV - PERSONAS 2021-2023.xlsx")
+with open("data/ubigeo_distrito.csv", 'rb') as file:
+        st.sidebar.download_button("Ubigeo", data = file, mime="text/csv",
+                                   file_name="ubigeo_distrito.csv")
+
+
 # Todos los filtros
-filters = filter_grav & filter_fecha
+filters = filter_grav & filter_fecha & filter_sex & filter_tipper
+
 
 # Medidas para Datos Filtrados
 st.write("Número de Datos con Filtros Aplicados")
@@ -50,40 +80,36 @@ col1.metric(label="Total de Personas", value=df.loc[filters].shape[0])
 col2.metric(label="Total de Siniestros", value=df.loc[filters].drop_duplicates(subset="CÓDIGO SINIESTRO").shape[0])
 st.divider()
 
+
 # Top Departamentos
 st.header(f"Top 10 Departamentos con más Personas Involucradas en Siniestros Fatales")
-st.caption(f"Filtro de Gravedad: {grav_pills}")
-st.caption(f"Filtro de Fecha: {slider_fecha[0]} - {slider_fecha[1]}")
 top_dep = df.loc[filters].groupby(by='DEPARTAMENTO').count().reset_index()
 top_dep = top_dep.nlargest(10, columns='CÓDIGO PERSONA')
 st.altair_chart(viz.barchart(top_dep, 'CÓDIGO PERSONA', 'DEPARTAMENTO', 'Número de Personas', 'Departamento', sorty='-x'))
 
+
 # Tipo de vehículo
 st.header(f"Distribución de Personas Involucradas en Siniestros Fatales por Tipo de Vehículo")
-st.caption(f"Filtro de Gravedad: {grav_pills}")
-st.caption(f"Filtro de Fecha: {slider_fecha[0]} - {slider_fecha[1]}")
-veh = df.groupby(by='VEHÍCULO').count().reset_index() 
+veh = df.loc[filters].groupby(by='VEHÍCULO').count().reset_index() 
 st.altair_chart(viz.barchart(veh, 'VEHÍCULO', 'CÓDIGO PERSONA', 'Tipo de Vehículo', 'Número de Personas', sortx='-y'))
+
 
 # Tipo de Siniestro
 st.header(f"Personas Involucradas en Siniestros Fatales por Clase de Siniestro")
-st.caption(f"Filtro de Gravedad: {grav_pills}")
-st.caption(f"Filtro de Fecha: {slider_fecha[0]} - {slider_fecha[1]}")
 tip_sin = df.loc[filters].groupby(by='CLASE DE SINIESTRO').count().reset_index()
 st.altair_chart(viz.pie(tip_sin, 'CLASE DE SINIESTRO', 'CÓDIGO PERSONA'))
 
+
 # Edad
 st.header(f"Distribución de la Edad Personas Involucradas en Siniestros Fatales por Clase de Siniestro")
-st.caption(f"Filtro de Gravedad: {grav_pills}")
-st.caption(f"Filtro de Fecha: {slider_fecha[0]} - {slider_fecha[1]}")
 st.altair_chart(viz.hist(df.loc[filters], 'EDAD', 'Edad', 'Número de Personas'))
+
 
 # Mapa
 map_per = viz.mapa(df.loc[filters])
 st.header(f"Mapa de Personas Involucradas en Siniestros Fatales")
-st.caption(f"Filtro de Gravedad: {grav_pills}")
-st.caption(f"Filtro de Fecha: {slider_fecha[0]} - {slider_fecha[1]}")
 st.components.v1.html(folium.Figure().add_child(map_per).render(), height=500)
+
 
 # Dataframe
 st.header("Tabla Filtrada")
