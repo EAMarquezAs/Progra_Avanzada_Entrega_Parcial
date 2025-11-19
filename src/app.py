f5558507f2a9f1ddd6e1a30aa86f6dd47d3db710
EAMarquezAs <ea.marquezas@alum.up.edu.pe>
2025-11-19 14:48:38 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -31,12 +31,16 @@ def update(w1, w2, range=False):
 df = get_data()
 df["fecha"] = pd.to_datetime(df["fecha"]).dt.date
 
+df_siniestros = get_data("siniestros", select="*")
+df_siniestros["fecha"] = pd.to_datetime(df_siniestros["fecha"]).dt.date
+
 
 # Título
-st.set_page_config(page_title="Personas - Siniestros Fatales (2021 - 2023)")
-st.title("Personas Involucradas en Siniestros Fatales (2021 - 2023)")
+st.set_page_config(page_title="Personas - Accidentes de Tránsito")
+st.title("Personas Involucradas en Accidentes de Tránsito")
 st.caption("Fuente ONSV: https://www.onsv.gob.pe/estaticos/excel/BBDD%20ONSV%20-%20PERSONAS%202021-2023.xlsx")
 st.caption("Fuente UBIGEO: https://github.com/jmcastagnetto/ubigeo-peru-aumentado/blob/main/ubigeo_distrito.csv")
+st.caption("Fuente Bomberos: https://sgonorte.bomberosperu.gob.pe/24horas")
 
 
 # Medidas
@@ -44,6 +48,12 @@ st.write("Número de Datos Antes de Procesamiento")
 col1, col2 = st.columns(2)
 col1.metric(label="Total de Personas", value=18464)
 col2.metric(label="Total de Siniestros", value=6705)
+st.metric(label="Número de Siniestros (últimas 24 horas)", value=df_siniestros.loc[df_siniestros["fecha"] >= datetime.date.today() - datetime.timedelta(days=1)].shape[0])
+
+
+# Botón para actualizar
+st.sidebar.write('Recolectar Datos de Base de los Bomberos')
+st.sidebar.button("Actualizar datos", on_click=update_data)
 
 
 # Filtro por Gravedad
@@ -72,8 +82,10 @@ causa_mulsel = st.sidebar.multiselect("Causa", options=map(lambda x: x.capitaliz
                                  placeholder="All")
 if len(causa_mulsel) == 0:
         filter_causa = True
+        filter_causa_sin = True
 else:
         filter_causa = df['causa'].isin(map(lambda x: x.upper(), causa_mulsel))
+        filter_causa_sin = df_siniestros['causa'].isin(map(lambda x: x.upper(), causa_mulsel))
 
 
 # Filtro por Fecha
@@ -86,10 +98,6 @@ slider_fecha = st.sidebar.slider(label='Fecha', min_value=datetime.date(2021, 1,
 box_fecha = st.sidebar.date_input(label="", min_value=datetime.date(2021, 1, 1), max_value=datetime.date(2023, 12, 31),
                                 key='boxfecha', on_change=update, args=('sliderfecha', 'boxfecha', True))
 filter_fecha = (df['fecha'] >= slider_fecha[0]) & (df['fecha'] <= slider_fecha[1])
-
-
-# Botón para actualizar
-st.sidebar.button("Actualizar datos", on_click=update_data)
 
 
 # Descargar archivos originales
@@ -109,42 +117,42 @@ filters = filter_grav & filter_fecha & filter_sex & filter_tipper & filter_causa
 
 # Medidas para Datos Filtrados
 st.write("Número de Datos con Filtros Aplicados")
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 col1.metric(label="Total de Personas", value=df.loc[filters].shape[0])
-col2.metric(label="Total de Siniestros", value=df.loc[filters].drop_duplicates(subset="cod_sin").shape[0])
+col2.metric(label="Total de Siniestros (base ONSV)", value=df.loc[filters].drop_duplicates(subset="cod_sin").shape[0])
+col3.metric(label="Total de Siniestros (base Bomberos)", value=df_siniestros.loc[(pd.to_datetime(df_siniestros["fecha"]).dt.year>=2024) & filter_causa_sin].shape[0])
 st.divider()
 
 
+
 # Top Departamentos
-st.header(f"Top 10 Departamentos con más Personas Involucradas en Siniestros Fatales")
+st.header(f"Top 10 Departamentos con más Personas Involucradas en Accidentes de Tránsito")
 top_dep = df.loc[filters].groupby(by='departamento').count().reset_index()
 top_dep = top_dep.nlargest(10, columns='cod_per')
 st.altair_chart(viz.barchart(top_dep, 'cod_per', 'departamento', 'Número de Personas', 'Departamento', sorty='-x'))
 
 
 # Tipo de vehículo
-st.header(f"Distribución de Personas Involucradas en Siniestros Fatales por Tipo de Vehículo")
+st.header(f"Distribución de Personas Involucradas en Accidentes de Tránsito por Tipo de Vehículo")
 veh = df.loc[filters].groupby(by='vehiculo').count().reset_index() 
 st.altair_chart(viz.barchart(veh, 'vehiculo', 'cod_per', 'Tipo de Vehículo', 'Número de Personas', sortx='-y'))
 
 
-# Tipo de Siniestro
-st.header(f"Personas Involucradas en Siniestros Fatales por Clase de Siniestro")
-tip_sin = df.loc[filters].groupby(by='clase_sin').count().reset_index()
-st.altair_chart(viz.pie(tip_sin, 'clase_sin', 'cod_per'))
-
-
 # Edad
-st.header(f"Distribución de la Edad Personas Involucradas en Siniestros Fatales por Clase de Siniestro")
+st.header(f"Distribución de la Edad Personas Involucradas en Accidentes de Tránsito por Clase de Siniestro")
 st.altair_chart(viz.hist(df.loc[filters], 'edad', 'Edad', 'Número de Personas'))
 
 
-# Mapa
+# Mapas
+map_sin = viz.mapa(df_siniestros.loc[(pd.to_datetime(df_siniestros["fecha"]).dt.year>=2024) & filter_causa_sin])
+st.header(f"Mapa de Accidentes de Tránsito (últimas 24 horas)")
+st.components.v1.html(folium.Figure().add_child(map_sin).render(), height=500)
+
 map_per = viz.mapa(df.loc[filters])
-st.header(f"Mapa de Personas Involucradas en Siniestros Fatales")
+st.header(f"Mapa de Personas Involucradas en Accidentes de Tránsito")
 st.components.v1.html(folium.Figure().add_child(map_per).render(), height=500)
 
 
 # Dataframe
-st.header("Tabla Filtrada")
+st.header("Tabla Filtrada de Personas")
 st.dataframe(df.loc[filters])
